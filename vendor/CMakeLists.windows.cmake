33d0adafbc4c8537cf21c32e4fe399c0e0dbcfb6
ntsakombungela2011-a11y <ntsakombungela2011@gmail.com>
2026-04-26 10:56:05 +0200

    hiihhih

--- vendor/CMakeLists.windows.cmake
+++ vendor/CMakeLists.windows.cmake
@@ -1,20 +1,19 @@
 # Windows-only cmake overlay for PolyLang vendor
 
-# Adds RUNTIME DESTINATION lib to all SHARED library install rules
-macro(install)
-    set(_args "${ARGN}")
-    list(FIND _args "TARGETS" _idx)
-    if(_idx EQUAL 0)
-        _install(${ARGN} RUNTIME DESTINATION lib)
-    else()
-        _install(${ARGN})
-    endif()
-endmacro()
-
 function(windows_vendor_fixes)
     if(TARGET quickjs)
         # Adds _USE_MATH_DEFINES so quickjs.h can find NAN on MSVC
         target_compile_definitions(quickjs PUBLIC _USE_MATH_DEFINES)
     endif()
+
+    # Get all targets in the current directory
+    get_property(targets DIRECTORY PROPERTY BUILDSYSTEM_TARGETS)
+    foreach(tgt IN LISTS targets)
+        get_target_property(type ${tgt} TYPE)
+        if(type STREQUAL "SHARED_LIBRARY")
+            # Adds RUNTIME DESTINATION lib to all SHARED library install rules
+            install(TARGETS ${tgt} RUNTIME DESTINATION lib)
+        endif()
+    endforeach()
 endfunction()
 cmake_language(DEFER CALL windows_vendor_fixes)
